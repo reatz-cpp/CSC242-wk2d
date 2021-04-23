364fe1a3cedab9f3e4811bbf9702225cc8fd5ab7
daniel.reatz <faceace69@gmail.com>
2021-04-22 21:18:05 -0500

    Added Circumference

--- circle.cpp
+++ circle.cpp
@@ -7,11 +7,13 @@ using namespace std;
 int main()
 {
 	double radius;
+	double circumference;
 	const double PI = 3.14159;
 	double area;
 	cout << "Enter radius\n";
 	cin >> radius;
 	area = PI * (radius * radius);
-	cout << "The area of a circle with radius " << radius << " is " << area << endl;
+	circumference = 2 * PI * radius;
+	cout << "The area and circumference of a circle with radius " << radius << " is " << area << " and " << circumference << endl;
 	return 0;
 }
